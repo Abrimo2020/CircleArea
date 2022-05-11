414a08aed07d10103554cf38791de7cbf245c108
Abrimo2020 <62276335+Abrimo2020@users.noreply.github.com>
2022-05-11 08:07:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CircleArea
+{
+    class CircleRectangle
+    {
+        double radius;
+
+        double pi;
+        public void AcceptDetails()
+        {
+            //length = 2; width = 3;
+            pi = 3.14;
+            Console.WriteLine("please Enter the radius of the Circle:");
+            radius = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+        public double getArea()
+        {
+            return radius * pi;
+        }
+        public void Display()
+        {
+            Console.WriteLine("The the radius of the circle:{0}", radius);
+            Console.WriteLine("The Area of circle:{0}", getArea());
+
+        }
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircleRectangle r = new CircleRectangle();
+            r.AcceptDetails();
+            r.Display();
+            //r.Display();
+            Console.ReadKey();
+        }
+    }
+}
